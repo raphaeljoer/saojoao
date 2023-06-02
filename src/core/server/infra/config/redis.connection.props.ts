1bc4023bb40c8c1648b7aa77f14eabe89ebdf181
Joer <ra.joer@gmail.com>
2023-06-02 20:10:33 -0300

    SM-015 feat(AddVoteUsecase): secondary audit-log implemented

--- src/core/server/infra/config/redis.connection.props.ts
+++ src/core/server/infra/config/redis.connection.props.ts
@@ -7,13 +7,3 @@ export const redisConnectionAuditLogProps = {
   host: process.env.SM_REDIS_AUDIT_LOG_HOST || '',
   token: process.env.SM_REDIS_AUDIT_LOG_TOKEN || ''
 };
-
-export const redisConnectionCounterTestProps = {
-  host: process.env.SM_REDIS_COUNTER_TEST_HOST || '',
-  token: process.env.SM_REDIS_COUNTER_TEST_TOKEN || ''
-};
-
-export const redisConnectionAuditLogTestProps = {
-  host: process.env.SM_REDIS_AUDIT_LOG_TEST_HOST || '',
-  token: process.env.SM_REDIS_AUDIT_LOG_TEST_TOKEN || ''
-};
